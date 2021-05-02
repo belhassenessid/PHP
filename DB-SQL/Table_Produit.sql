3b8486c51953a8632c699b673e5f2c4af8f7c38e
iheb09 <ihebetteyeb13@gmail.com>
2021-05-02 15:22:38 +0100

    table produit

--- DB-SQL/Table_Produit.sql
+++ DB-SQL/Table_Produit.sql
@@ -1,19 +1,41 @@
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
--- Table structure for table `product`
+-- Structure de la table `product`
+--
 
 CREATE TABLE `product` (
   `id` int(11) NOT NULL,
   `name` varchar(30) NOT NULL,
   `image` varchar(30) NOT NULL,
-  `price` int(11) NOT NULL
+  `price` int(11) NOT NULL,
+  `gender` char(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-INSERT INTO `product` (`id`, `name`, `image`, `price`) VALUES
-(1, 'product 1', '../images/prodHomme1.jpg', 10),
-(2, 'product 2', '../images/prodHomme2.jpg', 20),
-(4, 'PRODUCT 3', '../images/bershka1.jpg', 80),
-(5, 'PRODUCT 4', '../images/bershka2.jpg', 76),
+--
+-- Déchargement des données de la table `product`
+--
+
+INSERT INTO `product` (`id`, `name`, `image`, `price`, `gender`) VALUES
+(1, 'product 1', '../images/prodHomme1.jpg', 10, 'h'),
+(2, 'product 2', '../images/prodHomme2.jpg', 20, 'h'),
+(4, 'PRODUCT 3', '../images/bershka1.jpg', 80, 'h'),
+(5, 'PRODUCT 4', '../images/bershka2.jpg', 76, 'f');
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `product`
+--
+ALTER TABLE `product`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `product`
+--
+ALTER TABLE `product`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=66;
 COMMIT;
