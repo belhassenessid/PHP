5749ada9fc4497518ecdf79b2b54e5f9f43edade
Adouli Omar <omaradouli79@gmail.com>
2021-05-02 22:09:56 +0100

    design men.php changed

--- php/Homme.php
+++ php/Homme.php
@@ -88,7 +88,9 @@ if (openDropdown.classList.contains('show')) {
  </br></br></br>
 <!--affichage des produits -->
 <div class="small-container">
+  <hr id="top">
     <h2 class="title1">Men Products</h2>
+    <hr id="bottom">
 <div class="row">
   <?php
   $req = "select * from product where gender='h' order by id asc";
@@ -101,15 +103,17 @@ if (openDropdown.classList.contains('show')) {
         <div class="col-4">
             <form method="POST" action="Homme.php">
                     <div>
+                      <div class="div1">
                         <img src="<?php echo $row["image"];?>" class="img-responsive" style="width:200px;height:200px;"/><br/>
-                        <h4 class="text-info"><?php echo $row["name"];?></h4>
-                        <h4 class="text-danger"><?php echo "$".$row["price"];?></h4>
-                        <input type="text" name="item_quantity" class="form-contorl" value="1"/>
-                        <input type="hidden" name="item_id" value="<?php echo $row["id"];?>"/>
-                        <input type="hidden" name="item_name" value="<?php echo $row["name"];?>"/>
+                        <h4 class="prod-name"><?php echo $row["name"];?></h4>
+                        <h4 class="prod-price"><?php echo "$".$row["price"];?></h4>
+                        </div>
+                        <input type="number" max="30" min="1" step="1" name="item_quantity" class="prod-qn" value="1"/>
+                        <input type="hidden" name="item_id" value="<?php echo $row["id"];?>"/></h4>
+                        <input type="hidden" name="item_name" value="<?php echo $row["name"];?>"/></h4>
                         <input type="hidden" name="item_price" value="<?php echo $row["price"];?>"/>
                         <input type="hidden" name="item_image" value="<?php echo $row["image"];?>"/>
-                        </br><button type="submit" class="btn btn-success" name="add_to_cart">Add To Cart<i class="fas fa-shopping-cart"></i></button>
+                      </br><button type="submit" id="add" class="btn-add" name="add_to_cart">Add To Cart<i class="fas fa-shopping-cart"></i></button>
                     </div>
             </form>
             </div>
--- php/panier.php
+++ php/panier.php
@@ -24,6 +24,7 @@ if(isset($_GET['delete'])){
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 <link href="../style/panier.css?v=<?php echo time(); ?>" rel="stylesheet" type="text/css"/>
+
 <link rel="stylesheet" href="../style/all.min.css">
 </head>
 <body>
